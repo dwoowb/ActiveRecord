842b4603c0600332354b0b149781b78fe09d6fd6
App Academy Student <student@appacademy.io>
2014-03-21 17:08:53 -0400

    Initial commit lol

--- lib/active_record_lite/01_mass_object.rb
+++ lib/active_record_lite/01_mass_object.rb
@@ -1,16 +1,20 @@
 # deprecated for Rails 4
-# require_relative '00_attr_accessor_object.rb'
-#
-# class MassObject < AttrAccessorObject
-#   def self.my_attr_accessible(*new_attributes)
-#     # ...
-#   end
-#
-#   def self.attributes
-#     # ...
-#   end
-#
-#   def initialize(params = {})
-#     # ...
-#   end
-# end
+require_relative '00_attr_accessor_object.rb'
+
+class MassObject < AttrAccessorObject
+
+  def self.attributes
+    if self == MassObject
+      raise "must not call #attributes on MassObject directly"
+    else
+      []
+    end
+  end
+
+  def initialize(params = {})
+    params.each_pair do |name, value|
+      self.class.send(:my_attr_accessor, name.to_sym)
+      self.send("#{name.to_sym}=", value)
+    end
+  end
+end
--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -4,41 +4,79 @@ require 'active_support/inflector'
 
 class MassObject
   def self.parse_all(results)
-    # ...
+    objects = []
+    results.each do |hash|
+      objects << self.new(hash)
+    end
+    objects
   end
 end
 
 class SQLObject < MassObject
   def self.columns
-    # ...
+    columns = DBConnection.execute2("SELECT * FROM #{self.table_name}").first
+    columns.map(&:to_sym).each do |column|
+      self.my_attr_accessor(column)
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.underscore.pluralize
   end
 
   def self.all
-    # ...
+    result_hashes = DBConnection.execute(<<-SQL)
+                 SELECT
+                   *
+                 FROM
+                   #{@table_name}
+                 SQL
+    self.parse_all(result_hashes)
   end
 
   def self.find(id)
-    # ...
+    DBConnection.execute(<<-SQL, :id => id)
+    SELECT
+    *
+    FROM
+    #{@table_name}
+    WHERE
+    id = :id
+    SQL
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def insert
-    # ...
+    col_names = attributes.keys.join(", ")
+    col_values = attributes.values
+    question_marks = ["?"] * col_names.count
+
+    DBConnection.execute(<<-SQL, *col_values)
+    INSERT INTO
+      #{@table_name} (#{col_names})
+    VALUES
+      #{question_marks}
+    SQL
+      # last_insert_row_id
   end
 
-  def initialize
-    # ...
+  def initialize(params = {})
+    params.keys.each do |attr_name|
+      unless self.class.columns.include?(attr_name.to_sym)
+        p self.class.columns
+        p attr_name
+        raise "unknown attribute #{attr_name.to_s}"
+      end
+    end
+
+    super(params)
   end
 
   def save
